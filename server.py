9c8a99cce902ac409b2e04879f24a911ee3328c1
Cain96 <tk.777.k@gmail.com>
2017-07-07 03:33:50 +0900

    Complated

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ import sys, socket, select
 def broadcast(socklist, userdict, server_socket, sock, msg):
     # 配信処理
     for socket in socklist:
-        user = userdict[socket]
-        if socket != server_socket and sock:
+        if socket != server_socket and socket != sock:
+            user = userdict[sock]
             try:
                 msg = user + ">" + msg
                 socket.send(msg.encode())
@@ -30,52 +30,58 @@ def server():
     socklist.append(server_socket)
 
     while True:
-        read_sockets, write_sockets, error_sockets = select.select(socklist, [], [])
-        for sock in read_sockets:
-            if sock == server_socket:
+        try:
+            read_sockets, write_sockets, error_sockets = select.select(socklist, [], [])
+            for sock in read_sockets:
+                if sock == server_socket:
 
-                # 参加受付
-                new_sock, addr = server_socket.accept()
-                if len(socklist) < 5:
-                    new_sock.send(("REQUEST ACCEPTED\n").encode())
-                    print("REQUEST ACCEPTED\n")
-                    socklist.append(new_sock)
-                    broadcast(socklist, server_socket, new_sock, "[]")
+                    # 参加受付
+                    new_sock, addr = server_socket.accept()
+                    if len(socklist) < 5:
+                        new_sock.send(("REQUEST ACCEPTED\n").encode())
+                        print("REQUEST ACCEPTED")
+                        socklist.append(new_sock)
+                    else:
+                        new_sock.send(("REQUEST REJECTED\n").encode())
+                        print("REQUEST REJECTED")
+                        new_sock.close()
+                        break
+
+                    # ユーザ名登録
+                    user = ""
+                    while not user:
+                        user = new_sock.recv(1024).decode()
+                    user = (user.split("\n"))[0]
+                    if user in userdict.values():
+                        new_sock.send(("USERNAME REJECTED\n").encode())
+                        print("USERNAME REJECTED")
+                        socklist.remove(new_sock)
+                        new_sock.close()
+                        break
+                    new_sock.send(("USERNAME REGISTERED\n").encode())
+                    print("USERNAME REGISTERED")
+                    print("Join User : %s" % user)
+                    userdict.update({new_sock: user})
+                    broadcast(socklist, userdict, server_socket, new_sock, 'User Join\n')
+
                 else:
-                    new_sock.send(("REQUEST REJECTED\n").encode())
-                    print("REQUEST REJECTED\n")
-                    new_sock.close()
-                    break
-
-                # ユーザ名登録
-                user = ""
-                while not user:
-                    user = new_sock.recv(1024).decode()
-                user = (user.split("\n"))[0]
-                if user in userdict.values():
-                    new_sock.send(("USERNAME REJECTED\n").encode())
-                    print("USERNAME REJECTED\n")
-                    new_sock.close()
-                    break
-                new_sock.send(("USERNAME REGISTERED\n").encode())
-                print("USERNAME REGISTERED\n")
-                print("Join User : %s\n" % user)
-                userdict.update({new_sock: user})
-
-            else:
-                try:
-                    msg = sock.recv(1024).decode()
-                    if msg == "":
-                        raise Exception("Done")
-                    if msg:
-                        broadcast(socklist, userdict, server_socket, new_sock, msg)
-                except Exception as e:
-                    # 離脱処理
-                    print(e)
-                    broadcast(socklist, userdict, server_socket, sock, '[%s] Exit' % userdict[sock])
-                    userdict.pop(sock)
-                    sock.close()
-                    socklist.remove(sock)
+                    try:
+                        msg = sock.recv(1024).decode()
+                        if not msg:
+                            raise Exception
+                        else:
+                            broadcast(socklist, userdict, server_socket, sock, msg)
+                    except Exception as e:
+                        # 離脱処理
+                        print('[%s] Exit' % userdict[sock])
+                        broadcast(socklist, userdict, server_socket, sock, '[%s] Exit\n' % userdict[sock])
+                        userdict.pop(sock)
+                        sock.close()
+                        socklist.remove(sock)
+        except KeyboardInterrupt:
+            for sock in socklist:
+                sock.close()
+            sys.exit()
 
 
 if __name__ == '__main__':
